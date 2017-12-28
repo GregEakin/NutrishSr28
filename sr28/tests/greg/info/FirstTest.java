50522f4e4aeb565011199ed19f796752235ae97f
Greg Eakin <greg@gdbtech.info>
2017-12-28 08:09:52 -0800

    Setup JUnit test framework

---
+++ sr28/tests/greg/info/FirstTest.java
@@ -0,0 +1,11 @@
+package greg.info;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+    @Test
+    public void myFirstTest() {
+        Assertions.assertEquals(3, 2 + 1);
+    }
+}
