0eca06c1a5af8ae3f96b7d329ae1c12f29e09752
Greg Eakin <greg@gdbtech.info>
2017-12-28 20:53:26 -0800

    Move NutGroup over to the unit tests

--- sr28/tests/greg/info/dao/dataValadition/NutGroupTests.java
+++ sr28/tests/greg/info/dao/dataValadition/NutGroupTests.java
@@ -1,9 +1,13 @@
 package greg.info.dao.dataValadition;
 
+import greg.info.dao.entities.FoodDescription;
+import greg.info.dao.entities.FoodGroup;
 import greg.info.dao.utilities.NutrishRepositoryExtension;
 import org.hibernate.Session;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.Set;
 
 @ExtendWith(NutrishRepositoryExtension.class)
 public class NutGroupTests {
@@ -15,6 +19,11 @@ public class NutGroupTests {
 
     @Test
     public void Test1() {
-
+        FoodGroup foodGroup = session.load(FoodGroup.class, "1200");
+        System.out.println(foodGroup.getFdGrp_Cd() + ", " + foodGroup.getFdGrp_Desc());
+        Set<FoodDescription> foodDescriptionSet = foodGroup.getFoodDescriptionSet();
+        for (FoodDescription foodDescription : foodDescriptionSet) {
+            System.out.println("  " + foodDescription.getNDB_No() + " " + foodDescription.getShrt_Desc());
+        }
     }
 }
