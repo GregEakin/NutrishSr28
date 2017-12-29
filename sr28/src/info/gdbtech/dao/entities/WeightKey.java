23d48d9f50f15dfc0eebed560fb83caac103b26d
Greg Eakin <greg@gdbtech.info>
2017-12-28 22:16:13 -0800

    Update copyright

--- sr28/src/info/gdbtech/dao/entities/WeightKey.java
+++ sr28/src/info/gdbtech/dao/entities/WeightKey.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2017. Greg Eakin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package info.gdbtech.dao.entities;
 
 import javax.persistence.*;
@@ -45,10 +61,7 @@ public class WeightKey implements Serializable {
 
         if (foodDescription != null ? !foodDescription.equals(that.foodDescription) : that.foodDescription != null)
             return false;
-        if (Seq != null ? !Seq.equals(that.Seq) : that.Seq != null)
-            return false;
-
-        return true;
+        return Seq != null ? Seq.equals(that.Seq) : that.Seq == null;
     }
 
     @Override
